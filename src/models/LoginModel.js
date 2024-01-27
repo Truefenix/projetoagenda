09c0405a3a80d28f57da89e972162adc3fdb8288
fenix <fenix.azul09@protonmail.com>
2024-01-27 15:40:53 -0300

    hash da senha e testa se o email já existe

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -19,6 +20,15 @@ class Login {
     this.valida();
     if (this.errors.length > 0) return; // Verifica se há erros antes de prosseguir
 
+    await this.userExists();
+
+    if (this.errors.length > 0) return;
+
+    // bcryptjs
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    // banco de dados
     try {
       this.user = await LoginModel.create(this.body);
       console.log('Usuário criado com sucesso:', this.user);
@@ -28,6 +38,14 @@ class Login {
       } else {
         console.error('Erro ao criar usuário:', error);
       }
+    }
+  }
+
+  async userExists() {
+    // user -> email existe?
+    const user = await LoginModel.findOne({ email: this.body.email });
+    if(user) {
+      this.errors.push('Email já existe');
     }
   }
 
